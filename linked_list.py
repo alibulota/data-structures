1124773acf054f952aaa4087b22a430490bb6b4f
Alicija Bulota <aabulota@smcm.edu>
2015-02-08 20:19:20 -0800

    added method to insert node into linked list

--- linked_list.py
+++ linked_list.py
@@ -9,3 +9,8 @@ class LinkedList():
         self.head = None
         self.tail = 0
         self.size = 0
+
+    def insert_node(self, data):
+        inserted_node = Node(data)
+        inserted_node.next = self.head
+        self.head = inserted_node
