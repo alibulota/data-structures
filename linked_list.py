1771086b0dc28ad085c620b9f8d8cc67261f7527
Alicija Bulota <aabulota@smcm.edu>
2015-02-08 20:30:02 -0800

    added pop functionality

--- linked_list.py
+++ linked_list.py
@@ -14,3 +14,11 @@ class LinkedList():
         inserted_node = Node(data)
         inserted_node.next = self.head
         self.head = inserted_node
+
+    def pop_first_node(self):
+        if self.head is not None:
+            popped_data = self.head.data
+            self.head = self.head.next
+            return popped_data
+        else:
+            raise ValueError
