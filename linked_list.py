05f4cf03244be83cd5bf0df308f7fff2268f75c8
Alicija Bulota <aabulota@smcm.edu>
2015-02-09 00:29:47 -0800

    added correct file for linked list

--- linked_list.py
+++ linked_list.py
@@ -1,78 +1,77 @@
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Node(object):
+    def __init__(self, data, next_node=None):
+        self.data, self.next = data, next_node
 
 
-class LinkedList():
-    def __init__(self):
-        self.head = None
-        self.tail = 0
-        self.size = 0
+class LinkedList(object):
+    """Create and object-->linked_list."""
+    def __init__(self, head=None):
+        self.head = head
 
-    def insert_node(self, data):
+    def insert(self, data):
+        """Put the given value into Node at head of list."""
         inserted_node = Node(data)
         inserted_node.next = self.head
         self.head = inserted_node
 
-    def pop_first_node(self):
-        if self.head is not None:
-            popped_data = self.head.data
+    def pop(self):
+        """Return node data, remove node from head of list."""
+        if self.head:
+            value = self.head.data
             self.head = self.head.next
-            return popped_data
-        else:
-            raise ValueError
+            return value
 
-    def size_list(self):
+    def size(self):
+        """Return length of linked list."""
         current_size = self.head
-        size_count = 0
+        list_size = 0
         while current_size is not None:
-            size_count = size_count + 1
+            list_size = list_size + 1
             current_size = current_size.next
-        print size_count
-        return size_count
+        print list_size
+        return list_size
 
-    def search_node(self, data):
-        traverse = self.head
+    def search(self, data):
+        find_node = self.head
         found = False
-        while traverse is not None and not found:
-            if traverse.data == data:
+        while find_node is not None and not found:
+            if find_node.data == data:
                 found = True
-                print traverse.data
-                return traverse
+                print find_node.data
+                return find_node
             else:
-                traverse = traverse.next
+                find_node = find_node.next
 
-    def remove_node(self, data):
-        takeout = self.head
-        found = False
-        while takeout is not None and not found:
-            if takeout.data == data:
-                takeout == takeout.next.next
-                found = True
-                print takeout.data
-                return data
+    def remove(self, node):
+        """Remove node from list"""
+        if self.head:
+            prev_node = None
+            cur_node = self.head
+        else:
+            return None
+        while cur_node.next is not None:
+            if cur_node == node:
+                if prev_node is None:
+                    self.head = node.next
+                    return cur_node
+                elif cur_node.next is None:
+                    prev_node.next = None
+                    return cur_node
+                else:
+                    prev_node.next = cur_node.next
+                    return cur_node
+            else:
+                cur_node = cur_node.next
+        return None
 
-    def print_list(self):
-        """Print list tuple literal"""
-        tuple_literal_list = ()
-        all_node = self.head
-        while all_node is not None:
-            tuple_literal_list = tuple_literal_list + (all_node.data,)
-            all_node = all_node.next
-        print tuple_literal_list
-
-
-a = LinkedList()
-a.insert_node(3)
-a.insert_node(5)
-a.insert_node(6)
-a.insert_node(28)
-a.print_list()
-
-a.pop_first_node()
-a.print_list()
-a.size_list()
-a.print_list()
-a.remove_node(6)
-a.search_node(5)
+    def display(self):
+        """Print list in tuple literal format"""
+        # http://dbader.org/blog/functional-linked-lists-in-python
+        def internal_display(node):
+            if node is None:
+                return ""
+            elif node.next is None:
+                return str(node.data)
+            else:
+                return str(node.data) + ", " + internal_display(node.next)
+        return "(" + internal_display(self.head) + ")"
