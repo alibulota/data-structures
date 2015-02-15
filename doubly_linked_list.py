b75634a77d9a11b94c93c0b207d66969b03648ec
Alicija Bulota <aabulota@smcm.edu>
2015-02-15 11:26:53 -0800

    fixed remove method

--- doubly_linked_list.py
+++ doubly_linked_list.py
@@ -1,5 +1,6 @@
 # http://ls.pwd.io/2014/08/singly-and-doubly-linked-lists-in-python/
 
+
 class Node(object):
     def __init__(self, data):
         self.next = None
@@ -52,45 +53,21 @@ class DoublyList(object):
 
     def remove(self, data):
         """Remove first instance of val"""
-        curr_node = self.head
-        while curr_node is not None:
-            if curr_node.data == data:
-                #want to remove 
-                prev_node = curr_node.prev
-                next_node = curr_node.next
-                #first element in list
-                if self.head == curr_node:
-                    self.head = curr_node.next
-                elif self.tail == curr_node:
-                    self.tail = curr_node.prev
+        if self.head is None:
+            raise LookupError('Data not in list.')
+        elif self.head.data == data:
+            self.head = self.head.next
+        elif self.tail.data == data:
+            self.tail = self.tail.prev
+        else:
+            curr_node = self.head
+            while True:
+                if curr_node.data == data:
+                    curr_node.prev.next = curr_node.next
+                    curr_node.next.prev = curr_node.prev
+                    break
                 else:
-                    prev_node.next = next_node
-                    next_node.prev = prev_node
-                    curr_node = curr_node.next
-        return LookupError("Data not in list.")
-
-
-
-    #     def search(self, data):
-    #     find_node = self.head
-    #     while find_node is not None:
-    #         if find_node.data == data:
-    #             return find_node
-    #         else:
-    #             find_node = find_node.next
-    #
-    # def remove(self, node):
-    #     """Remove node from list"""
-    #     prev_node = None
-    #     cur_node = self.head
-    #     while cur_node is not None:
-    #         if cur_node == node:
-    #             if prev_node is None:
-    #                 self.head = node.next
-    #                 return cur_node
-    #             else:
-    #                 prev_node.next = cur_node.next
-    #             return cur_node
-    #         else:
-    #             cur_node = cur_node.next
-    #     return None
+                    if curr_node.next is None:
+                        raise LookupError('Data not in list.')
+                    else:
+                        curr_node = curr_node.next
