0a02796c81ec65280253a6beca2d238585aa56e2
Alicija Bulota <aabulota@smcm.edu>
2015-02-16 17:40:44 -0800

    corrected tests for push and pop

--- test_stack.py
+++ test_stack.py
@@ -1,27 +1,26 @@
 from stack import Stack
 import pytest
+import sys
 
 
-class StackTest():
+def test_init():
+    stack = Stack()
+    assert stack is not None
 
-    def test_init(self):
-        stack = Stack()
-        assert stack is not None
+def test_push():
+    stack = Stack()
+    stack.push(3)
+    stack.push(2)
+    stack.push(1)
+    assert stack.pop() == 1
+    assert stack.pop() == 2
+    assert stack.pop() == 3
 
-    def test_push():
-        stack = Stack()
-        stack.push(3)
-        stack.push(2)
-        stack.push(1)
-        assert stack.pop() == 1
-        assert stack.pop() == 2
-        assert stack.pop() == 3
-
-    def test_pop():
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        assert stack.pop() == 3
-        assert stack.pop() == 2
-        assert stack.pop() == 1
+def test_pop():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    assert stack.pop() == 3
+    assert stack.pop() == 2
+    assert stack.pop() == 1
