deef18dccdc3dfe5ffaa3808db2adcf44c798663
Alicija Bulota <aabulota@smcm.edu>
2015-03-18 02:15:16 -0700

    added name main function with time it. Best case is times better than worst case (expected)

--- quick_sort.py
+++ quick_sort.py
@@ -18,14 +18,24 @@ def quick_sort(data):
         return small_data + pivot_list + big_data
 
 
+if __name__ == '__main__':
+    import timeit
 
-        # for i, val in enumerate(data):
-        #     if i != pivot:
-        #         if val < data[pivot]:
-        #             small_data.append(val)
-        #         else:
-        #             big_data.append(val)
+    best = [i for i in xrange(1, 500, 1)]
+    worst = [i for i in xrange(-1, 500, 1)]
 
-        # quick_sort(small_data)
-        # quick_sort(big_data)
-        # data[:] = small_data + [data[pivot]] + big_data
+    def best_case():
+        return quick_sort(best)
+
+    def worst_case():
+        return quick_sort(worst)
+
+    print "Best Case: {}".format(
+        timeit.timeit('best_case()', setup='from __main__ import best_case', number=1000))
+
+    print "Worst Case: {}".format(
+        timeit.timeit('worst_case()', setup='from __main__ import worst_case', number=1000))
+
+
+# Best Case: 30.3696181774
+# Worst Case: 30.6029648781
