d0057d6c6ea1716198fcfb93ebb2c1e56d5d66fa
Alicija Bulota <aabulota@smcm.edu>
2015-03-17 20:06:58 -0700

    added name main with time it tests

--- insertion_sort.py
+++ insertion_sort.py
@@ -9,3 +9,20 @@ def sort(list):
         list[j] = x
 
 
+if __name__ == '__main__':
+    import timeit
+
+    best = [i for i in range(1000)]
+    worst = [i for i in range(1000)][::-1]  # reverse
+
+    def best_case():
+        return sort(best)
+
+    def worst_case():
+        return sort(worst)
+
+    print "Best Case: {}".format(
+        timeit.timeit('best_case()', setup='from __main__ import best_case', number=100))
+
+    print "Worst Case: {}".format(
+        timeit.timeit('worst_case()', setup='from __main__ import worst_case', number=100))
