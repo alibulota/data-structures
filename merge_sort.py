8920aed068a0ba85f2f8d6345e7c50760f7e7f68
Alicija Bulota <aabulota@smcm.edu>
2015-03-17 22:58:35 -0700

    added name main function to add infile testing with time it:

--- merge_sort.py
+++ merge_sort.py
@@ -25,3 +25,21 @@ def merge_sort(a_list):
                 r_side += 1
             else:
                 raise Exception("Can not merge")
+
+if __name__ == '__main__':
+    import timeit
+
+    best = [i for i in range(1000)]
+    worst = [i for i in range(1000)][::-1]  # reverse
+
+    def best_case():
+        return merge_sort(best)
+
+    def worst_case():
+        return merge_sort(worst)
+
+    print "Best Case: {}".format(
+        timeit.timeit('best_case()', setup='from __main__ import best_case', number=100))
+
+    print "Worst Case: {}".format(
+        timeit.timeit('worst_case()', setup='from __main__ import worst_case', number=100))
